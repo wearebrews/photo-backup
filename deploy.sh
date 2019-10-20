6a2155a7c8bf40117e68db638188c67d31f677e0
Håvard Skåra Mellbye <havardmellbye@gmail.com>
2019-10-20 21:17:17 +0200

    Update deploy image name

--- deploy.sh
+++ deploy.sh
@@ -5,5 +5,5 @@ NAMESPACE=photo-backup
 echo "$DOCKER_PASSWORD" | docker login -u "$DOCKER_USERNAME" --password-stdin
 make docker-push
 kubectl apply -n $NAMESPACE -f kubernetes/
-kubectl set image -n $NAMESPACE deployment/receiver receiver=wearebrews/receiver:$VERSION 
+kubectl set image -n $NAMESPACE deployment/receiver receiver=wearebrews/photo-backup-receiver:$VERSION 
 
